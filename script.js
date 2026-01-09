044f4c634e47900a3349408197415d86fd1c1a2c
Prathima S <52801848-prathima01@users.noreply.replit.com>
2026-01-09 09:53:05 +0000

    day to update

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log("js is working");
 //alert("js is link");
 console.log("day 2 started ");
 console.log("git hub connected ");
+
+console.log("second push test ");
+
+
+console.log("change check 123");
+
