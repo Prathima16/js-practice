3c3ee44d8d9965bb3de717ea17261936e3163941
Prathima S <52801848-prathima01@users.noreply.replit.com>
2026-01-12 16:25:51 +0000

    Updated Js Fundamentals

--- jsfundamentals.js
+++ jsfundamentals.js
@@ -79,11 +79,67 @@ if(aagee>=18){
 } 
 //if else if statement
 
-const num = Number(readline.question("Enter a number: "));
-const remaiderAfterdivisionByThree = num%3;
-const remaiderAfterdivisionByFive = num%5;
+const no= Number(readline.question("Enter a number: "));
+const remaiderAfterdivisionByThree = no%3;
+const remaiderAfterdivisionByFive = no%5;
 if(remaiderAfterdivisionByThree===0){
   console.log("fizz");
 }else  if(remaiderAfterdivisionByFive===0){
   console.log("buzz");
+}
+
+//Logical operators
+//AND(&&),OR(||),NOR
+const remaiderBy2 = no%2;
+const remaiderBy6 = no%6;
+const remaiderBy7 = no%7;
+if(remaiderBy2===0 && remaiderBy6===0){
+  console.log("fizz");
+  
+}else  if(remaiderBy2===0 || remaiderBy6===0){
+  console.log("buzz");
+} else if(remaiderBy7===0){
+  console.log("Fizzbuzz")
+}else{
+  console.log("not divisible by 2,6,7");
+}
+
+//Nested conditional statements
+const  numA = Number(readline.question("Enter a number: "));
+const remainder = numA%2;
+if(remainder===0){
+  console.log(`${numA} is even`);
+  if (numA%4===0){
+    console.log(`${numA} is divisible by 4`);
+  }else{
+    console.log(`${numA} is not divisible by 4`);
+  }
+}else{
+  console.log(`${numA} is an odd number`);
+  if(numA%5===0){
+    console.log(`${numA} is divisible by 5`);
+  }else{
+    console.log(`${numA} is not divisible by 5`);
+  }
+}
+
+//Exercises
+
+//1 print the smallest string among 3 strings
+const fstring="Apple";
+const sstring="Banana";
+const tstring="Watermelon";
+
+const  fstringLength = fstring.length;
+const  sstringLength = sstring.length;
+const  tstringLength = tstring.length;
+
+if(fstringLength<sstringLength && fstringLength<tstringLength){
+  console.log(`${fstring} is the smallest string`);
+}else if(sstringLength<fstringLength && sstringLength<tstringLength){
+   console.log(`${sstring} is the smallest string`);
+}else if(tstringLength<fstringLength && tstringLength<sstringLength){
+   console.log(`${tstring} is the smallest string`);
+}else{
+  console.log("All strings are of equal length");
 }
